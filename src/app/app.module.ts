4c555a15a7cd7a7727205ef49f3599c14ccd18b2
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2022-12-09 21:23:21 +0700

    float commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FloatComponent } from './components/float/float.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FloatComponent
   ],
   imports: [
     BrowserModule,
