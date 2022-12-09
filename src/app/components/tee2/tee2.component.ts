8a52438967e308a281447f9b06f5e72ddb6704a2
charat <charatkosit@gmail.com>
2022-12-09 21:41:15 +0700

    add tee2 component

---
+++ src/app/components/tee2/tee2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tee2',
+  templateUrl: './tee2.component.html',
+  styleUrls: ['./tee2.component.css']
+})
+export class Tee2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
